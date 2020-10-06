029e72c07a5cb506d2aa0146df2e6003288bdb31
Justin Kim <neibc96@gmail.com>
2020-10-06 23:25:55 +0900

    Update deepracer7.py

--- deepracer7.py
+++ deepracer7.py
@@ -33,12 +33,13 @@ def reward_function(params):
 
     SPEED_THRESHOLD_1 = 0.5
     SPEED_THRESHOLD_2 = 3.5
-    
+    SPEED_THRESHOLD_DIFF = SPEED_THRESHOLD_2 - SPEED_THRESHOLD_1
+
     if reward > 0:
         if params['speed'] < SPEED_THRESHOLD_1:
             reward *= 0.6
         elif params['speed'] <= SPEED_THRESHOLD_2:
-            reward *= (0.6 + 0.4 * (params['speed'] - 0.5) / 3.0)
+            reward *= (0.6 + 0.4 * (params['speed'] - SPEED_THRESHOLD_1) / SPEED_THRESHOLD_DIFF)
 
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 8
